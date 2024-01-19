784697238b906502c3fbe98d4967ef3eacdbd2cb
Parker <p.cronin.connolly@gmail.com>
2024-01-19 14:22:19 -0500

    added some styling to concert-card to put them in a row on top and make images smaller

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ fetch("https://www.jambase.com/jb-api/v1/events?apikey=93f5cc80-f6d0-4f03-a4c7-f
 fetch('http://localhost:3000/featured-events')
 .then(resp => resp.json())
 .then(featuredEventsArr => renderFeaturedEvents(featuredEventsArr))
-
+// taking in the array from our local fetch
+// creating a container that will point to the div featured-events 
+// can style the look of each event here
+// iterate through the array and create a concert card for each object
+// create a variable that creates space for each data in the DOM each loop
+// set the appropriate attribute of each variable to the data in the object
+// append the data to the container
+//append the container to the featured-events div 
 function renderFeaturedEvents(featuredEventsArray) {
     const featuredCon = document.querySelector('#featured-events')
 
-    const featuredDiv = document.createElement('div')
-    featuredDiv.className = "concert-card"
+    
 
     featuredEventsArray.forEach((featuredObj) => {
+        const featuredDiv = document.createElement('div')
+        featuredDiv.className = "concert-card"
     const featName = document.createElement('h2')
     const featArtist = document.createElement('h2')
     const featVenue = document.createElement('h3')
@@ -25,13 +33,8 @@ function renderFeaturedEvents(featuredEventsArray) {
     featDate.textContent = featuredObj.date
     featImg.src = featuredObj.image
 
-
-    featuredDiv.appendChild(featName)
-    featuredDiv.appendChild(featArtist)
-    featuredDiv.appendChild(featVenue)
-    featuredDiv.appendChild(featDate) 
-    featuredDiv.appendChild(featImg)
-    featuredCon.appendChild(featuredDiv)
+        featuredDiv.append(featName, featArtist, featVenue, featDate, featImg)
+        featuredCon.appendChild(featuredDiv)
 
 
 
