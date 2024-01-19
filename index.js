b467f0919dac883fbed16d5b11c6dddf029454a6
Parker <p.cronin.connolly@gmail.com>
2024-01-19 12:22:40 -0500

    db.json formatted properly

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ fetch("https://www.jambase.com/jb-api/v1/events?apikey=93f5cc80-f6d0-4f03-a4c7-f
 .then(resp => resp.json())
 .then(data => console.log(data))
 
+fetch('http://localhost:3000/featured-events')
+.then(resp => resp.json())
+.then(data => console.log(data))
+function renderFeaturedEvents(featuredEventsArr) {
 
+
+}
